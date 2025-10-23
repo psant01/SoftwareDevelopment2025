ee560232d65bd7454c491dec8d7f7b9a63760962
Paul Sant <45962682+psant01@users.noreply.github.com>
2025-10-23 12:25:26 +0100

    Unit 1 Consolidate Task 2

---
+++ Consolidate/unit1/ConsolidateTask2.py
@@ -0,0 +1,5 @@
+# A simple Python program that prints Hello World and I love Python
+
+print("Welcome to the Software Development unit")
+print("Hello, my name is Paul Sant")
+print("Learning Python is easy, and I am really enjoying it!")
