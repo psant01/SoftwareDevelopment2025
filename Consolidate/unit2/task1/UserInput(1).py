208e9658ee52f2b78cdadd51824af35572d78d0d
Paul Sant <45962682+psant01@users.noreply.github.com>
2025-10-23 13:53:41 +0100

    Adding a Python src code file
    Uploading a Python Src code file that demonstrates basic Python programming.

---
+++ Consolidate/unit2/task1/UserInput(1).py
@@ -0,0 +1,10 @@
+# A simple Python  program that makes use
+# of the in-built input() function to get input
+# from the user.
+# By default, this is treated as text
+
+# Get input from the user and store in myVariable
+myVariable = input("Please enter your name: ")
+
+# Use the content of the variable to print a welcome message
+print(f'Hello {myVariable} it is very nice to meet you!')
